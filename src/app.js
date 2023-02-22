76c274a7aece97ce60fbebbea34dde3fc09766fe
MMeitin <mario.meitin.alvarez@gmail.com>
2023-02-22 15:05:39 +0000

    feat: Added random card generator. Pending fix button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  const iconos = document.querySelector(".number");
+  const boton = document.querySelector(".boton");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const colores = ["spades", "clubs", "hearts", "diamonds"];
+  const generarCarta = () => {
+    const randomNumber = () => {
+      let randomV = Math.floor(Math.random() * valores.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+      return valores[randomV];
+    };
+
+    const randomSuit = () => {
+      let randomC = Math.floor(Math.random() * colores.length);
+
+      return colores[randomC];
+    };
+
+    let color = randomSuit();
+
+    if (color == "hearts" || color == "diamonds") {
+      iconos.style.color = "red";
+      iconos.style.border = "3px groove red";
+    } else {
+      iconos.style.border = "3px groove black";
+      iconos.style.color = "black";
+    }
+
+    iconos.classList.add(color);
+    iconos.innerHTML = randomNumber();
+  };
+
+  generarCarta();
+
+  boton.addEventListener("click", generarCarta);
 };
